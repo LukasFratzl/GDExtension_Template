f1873465df0880d8a15d624ec45f956116249330
flow____ <lukas.fratzl@gmail.com>
2024-04-28 12:13:04 +0200

    Used Clang Format code style

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -16,28 +16,33 @@
 using namespace godot;
 
 void initialize_test_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
+  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+    return;
+  }
 
-	GDREGISTER_CLASS(MyCustomNode);
+  GDREGISTER_CLASS(MyCustomNode);
 }
 
 void uninitialize_test_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
+  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+    return;
+  }
 }
 
 extern "C" {
 // Initialization.
-GDExtensionBool GDE_EXPORT test_module_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+GDExtensionBool GDE_EXPORT
+test_module_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
+                         GDExtensionClassLibraryPtr p_library,
+                         GDExtensionInitialization *r_initialization) {
+  godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library,
+                                                 r_initialization);
 
-	init_obj.register_initializer(initialize_test_module);
-	init_obj.register_terminator(uninitialize_test_module);
-	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+  init_obj.register_initializer(initialize_test_module);
+  init_obj.register_terminator(uninitialize_test_module);
+  init_obj.set_minimum_library_initialization_level(
+      MODULE_INITIALIZATION_LEVEL_SCENE);
 
-	return init_obj.init();
+  return init_obj.init();
 }
 }
